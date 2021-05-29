d6c6809ac869837ea9ced42853385ee63e42cf5b
Yarik <vitkin@ex.ua>
2021-05-29 16:57:14 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    async function getAllEmployees() {
+        const res = await axios.get("https://localhost:5001/api/Employees/GetEmployees");
+        console.log(res);
+    }
+
+    getAllEmployees();
+
+    return (
+        <div className="App">
+            Hello
+        </div>
+    );
 }
 
 export default App;
